019b5b9d07b32bd644f50bb37463ec27f8f24b34
gwonjunhyeok <junekk8722@gmail.com>
2025-07-28 13:11:19 +0900

    0728

---
+++ Assets/script/DragData.cs
@@ -0,0 +1,8 @@
+public class DragData
+{
+    public bool fromMain; // true¸é mainItems, false¸é subItems
+    public int originIndex;
+    public ItemStack draggedItem;
+    public int draggedCount;
+    public bool isSplit;
+}
