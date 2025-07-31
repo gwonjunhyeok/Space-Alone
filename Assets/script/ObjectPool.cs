f1d6c007c34edc25aec7c430cfb52ebdf9c1c41c
gwonjunhyeok <junekk8722@gmail.com>
2025-07-31 19:21:54 +0900

    몬스터 작업 시작
    AI NavMesh 작업 시작

--- Assets/script/ObjectPool.cs
+++ Assets/script/ObjectPool.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class ObjectPool : MonoBehaviour
 {
+    [Header("Basic Gun Pool")]
     public static ObjectPool Instance;
     public int poolSize = 30;
     private List<GameObject> bulletPool;
     public GameObject bulletPrefab;
+
+    [Header("Monster Pool")]
+    public int MonsterPoolSize = 10;
+    public GameObject MonsterPrefab;
+    private List<GameObject> MonsterPool;
     private void Awake()
     {
         Instance = this;
@@ -22,6 +28,14 @@ public class ObjectPool : MonoBehaviour
             GameObject bullet = Instantiate(bulletPrefab);
             bullet.SetActive(false);
             bulletPool.Add(bullet);
+        }
+        MonsterPool = new List<GameObject>();
+        //풀 사이즈만큼 생성
+        for (int i = 0; i < MonsterPoolSize; i++)
+        {
+            GameObject monster = Instantiate(MonsterPrefab);
+            monster.SetActive(false);
+            MonsterPool.Add(monster);
         }
     }
 
@@ -38,6 +52,18 @@ public class ObjectPool : MonoBehaviour
             {
                 bullet.SetActive(true);
                 return bullet;
+            }
+        }
+        return null;
+    }
+    public GameObject GetMonster()
+    {
+        foreach (var m in MonsterPool)
+        {
+            if (!m.activeInHierarchy)
+            {
+                m.SetActive(true);
+                return m;
             }
         }
         return null;
